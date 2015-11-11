bb18bc001309671f49808412799ab6455ee82a1c
Ivan Nikitin <vantoozz@gmail.com>
2015-11-11 22:34:57 +0700

    + routes test

---
+++ tests/Http/RoutesTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http;
+
+
+use App\Resources\User;
+use App\TestCase;
+use Illuminate\Contracts\Auth\Guard;
+
+class RoutesTest extends TestCase
+{
+
+    /**
+     * @test
+     */
+    public function it_handle_root()
+    {
+        $this->withoutMiddleware();
+        /** @var Guard $auth */
+        $user = new User;
+        $user->setId(12334567);
+        $this->be($user);
+        $this->visit('/')->see('\'id\' => 12334567');
+    }
+
+    /**
+     * @test
+     */
+    public function it_handle_get_app()
+    {
+        $this->withoutMiddleware();
+        /** @var Guard $auth */
+        $user = new User;
+        $user->setId(12334567);
+        $this->be($user);
+        $this->visit('/app/some_provider')->see('\'id\' => 12334567');
+    }
+
+}

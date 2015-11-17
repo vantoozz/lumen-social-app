0536db993824275219e76f25ce6d688065cc9b0f
Ivan Nikitin <vantoozz@gmail.com>
2015-11-17 22:32:29 +0700

    + whoops service provider test

---
+++ tests/Providers/VKServiceProviderTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Providers;
+
+use App\Social\Provider\VK;
+use App\TestCase;
+
+class VKServiceProviderTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_provides_vk()
+    {
+        $this->refreshApplication();
+        $provider = new VKServiceProvider($this->app);
+        static::assertSame(['social.vk'], $provider->provides());
+    }
+
+    /**
+     * @test
+     */
+    public function it_registers_db_connection()
+    {
+        $this->refreshApplication();
+        $provider = new VKServiceProvider($this->app);
+        $provider->register();
+        static::assertInstanceOf(VK::class, $this->app->make('social.vk'));
+    }
+}
---
+++ tests/Providers/WhoopsServiceProviderTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Providers;
+
+use App\TestCase;
+use Whoops\Run;
+
+class WhoopsServiceProviderTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_provides_whoops()
+    {
+        $this->refreshApplication();
+        $provider = new WhoopsServiceProvider($this->app);
+        static::assertSame([Run::class], $provider->provides());
+    }
+
+    /**
+     * @test
+     */
+    public function it_registers_users_repository()
+    {
+        $this->refreshApplication();
+        $provider = new WhoopsServiceProvider($this->app);
+        $provider->register();
+        static::assertInstanceOf(Run::class, $this->app->make(Run::class));
+    }
+}

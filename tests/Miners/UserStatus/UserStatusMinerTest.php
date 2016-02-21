793cd38c8a4c775be120d024077ef084a3ffbc4b
Ivan Nikitin <vantoozz@gmail.com>
2016-02-21 20:48:27 +0700

    + user status miner test

---
+++ tests/Miners/UserStatus/UserStatusMinerTest.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Miners\UserStatus;
+
+use App\Activities\ActivityType;
+use App\Activities\UserActivity;
+use App\Exceptions\NotFoundInRepositoryException;
+use App\Repositories\UserActivity\UserActivityRepositoryInterface;
+use App\Resources\User;
+use App\TestCase;
+use Carbon\Carbon;
+
+class UserStatusMinerTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_mark_user_outdated_if_user_has_no_last_name()
+    {
+        $repository = static::getMock(UserActivityRepositoryInterface::class);
+        /** @var  UserActivityRepositoryInterface $repository */
+        $miner = new UserStatusMiner($repository);
+        $user = new User;
+        $user->setLastName('');
+        static::assertTrue($miner->isUserInfoOutdated($user));
+    }
+
+    /**
+     * @test
+     */
+    public function it_mark_user_outdated_if_user_has_no_photo()
+    {
+        $repository = static::getMock(UserActivityRepositoryInterface::class);
+        /** @var  UserActivityRepositoryInterface $repository */
+        $miner = new UserStatusMiner($repository);
+        $user = new User;
+        $user->setLastName('last name');
+        $user->setPhoto('');
+        static::assertTrue($miner->isUserInfoOutdated($user));
+    }
+
+    /**
+     * @test
+     */
+    public function it_mark_user_outdated_if_user_last_activity_is_too_old()
+    {
+        $repository = static::getMock(UserActivityRepositoryInterface::class);
+
+        $user = new User;
+        $user->setLastName('last name');
+        $user->setPhoto('photo');
+        $user->setId(123);
+
+        $activity = new UserActivity;
+        $activity->setDatetime(new Carbon('2001-01-01'));
+        $repository
+            ->expects(static::once())
+            ->method('getActivity')
+            ->with(new ActivityType(ActivityType::SYNC), 123)
+            ->willReturn($activity);
+
+        /** @var  UserActivityRepositoryInterface $repository */
+        $miner = new UserStatusMiner($repository);
+        static::assertTrue($miner->isUserInfoOutdated($user));
+    }
+    /**
+     * @test
+     */
+    public function it_mark_user_outdated_if_no_user_activity()
+    {
+        $repository = static::getMock(UserActivityRepositoryInterface::class);
+
+        $user = new User;
+        $user->setLastName('last name');
+        $user->setPhoto('photo');
+        $user->setId(123);
+
+        $repository
+            ->expects(static::once())
+            ->method('getActivity')
+            ->with(new ActivityType(ActivityType::SYNC), 123)
+            ->willThrowException(new NotFoundInRepositoryException);
+
+        /** @var  UserActivityRepositoryInterface $repository */
+        $miner = new UserStatusMiner($repository);
+        static::assertTrue($miner->isUserInfoOutdated($user));
+    }
+}
